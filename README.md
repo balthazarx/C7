2b85ee882190384748014f36bc3406766a5deba9
Logan Smith <logansmt@gmail.com>
2025-01-14 20:42:24 -0700

    code

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Challenge 7
+
+## Description
+To create a README using code.
+
+## Installation
+Run the commmand "npm install" then followed by "node index.js". Afterwards you will be able to input the prompts the code asks.
+
+## Usage
+N/A
+
+## Contributing
+N/A
+
+## Tests
+N/A
+
+## License
+This project is licensed under the MIT license.
+
+## Questions
+GitHub: [balthazarx](https://github.com/balthazarx)
+Email: [google@google.com](mailto:google@google.com)
+      
--- index.js
+++ index.js
@@ -1,13 +1,106 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`${fileName} generated successfully!`);
+      }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+const init = async () => {
+    try {
+      const answers = await inquirer.prompt(questions);
+    
+      // Combine user answers into a markdown format
+      const data = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+GitHub: [${answers.github}](https://github.com/${answers.github})
+Email: [${answers.email}](mailto:${answers.email})
+      `;
+
+      // Write the README content to the file
+      writeToFile('README.md', data); // Pass the constructed data here
+    } catch (err) {
+      console.error('Error generating README:', err);
+    }
+};
 
 // Function call to initialize app
 init();
